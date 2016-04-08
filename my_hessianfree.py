151d7474e7f3be34bcc235c72c2b6e47a689dece
Thomas Wood <odellus@users.noreply.github.com>
2016-04-08 12:59:54 -0700

    Update my_hessianfree.py
    :construction: Learning... :construction:

--- my_hessianfree.py
+++ my_hessianfree.py
@@ -24,9 +24,17 @@ def hess_free(f, x0, y, max_steps=5000, tol=1e-6):
     # step a distance e0 in two directions, find gradients there
     _, df_x = f(x0+ e0*x0, y)
     _, df_d = f(x0+ e0*d0, y)
+    
+    ##########################################################################
+    ##!! NO NO NO. This is all wrong. Why not take a look at some other   !!##
+    ##!! people's code and see how you can use forward automatic          !!##
+    ##!! differentiation to calculate this value instead of approximating !!##
+    ##!! it with finite differences!                                      !!##
+    ##########################################################################
+    
     # approximate directional derivative of objective function along x0 and d0
-    Hx = (df_x - df0)/e0 # product of hessian and x0
-    Hd = (df_d - df0)/e0 # product of hessian and d0
+    # Hx = (df_x - df0)/e0 # product of hessian and x0
+    #Hd = (df_d - df0)/e0 # product of hessian and d0
     # Calculate optimal initial step size once outside loop with bootstrap Hd
     a = - np.dot(d0.T, Hx0 + df0) / np.dot(d0.T, Hd)
     d = d0
